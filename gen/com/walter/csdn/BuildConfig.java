72b19b09ef53865f8c36a77f8d7837f7755b53b0
LoveWalter <walter080393@gmail.com>
2015-11-24 14:42:47 +0800

    Revert "Simple CSDN blogger"
    This reverts commit fdb6f907fe606e0a7da760072ef179ce65b7794b.

--- gen/com/walter/csdn/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.walter.csdn;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/walter/csdn/csdn/Constaint.java
+++
@@ -1,9 +0,0 @@
-package com.walter.csdn.csdn;
-
-public abstract interface Constaint {
-	public static final int NEWS_TYPE_YEJIE = 1;
-	public static final int NEWS_TYPE_YIDONG = 2;
-	public static final int NEWS_TYPE_YANFA = 3;
-	public static final int NEWS_TYPE_CHENGXUYUAN = 4;
-	public static final int NEWS_TYPE_YUNJISUAN = 5;
-}
